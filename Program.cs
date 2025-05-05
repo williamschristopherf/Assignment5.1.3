8283a16fb772c3d3025db42f1511b7ce4018bad6
Christopher Williams <williamstophef@gmail.com>
2025-05-05 13:43:13 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿static bool ArrayDuplicates(int[] myarr)
+{
+    Dictionary<int, int> duplicates = new Dictionary<int, int>();
+    for (int i = 0; i < myarr.Length; i++)
+    {
+        if (duplicates.ContainsKey(myarr[i]))
+        {
+            return true;
+        }
+        else
+        {
+            duplicates[myarr[i]] = 1;
+        }
+    }
+    return false;
+}
+
+Console.WriteLine(ArrayDuplicates([1,2,3,4]));
+Console.WriteLine(ArrayDuplicates([1,4,3,3,6,8,9]));
